d8f76381cb2a6904fef09a4e118d192637ea6dae
shurik <shurik@skotch.nirvana>
2022-02-07 17:45:50 +0300

    asyncio

--- nasa_api/nasa_api/find_products.py
+++ nasa_api/nasa_api/find_products.py
@@ -1,18 +1,23 @@
 
 import requests
 
-
+import asyncio
+import functools
 
 
 class Products:
     __singleton = None
     __url = 'https://lpdaacsvc.cr.usgs.gov/appeears/api/product'
     __json_tokens = {'Description':'Elevation', 'Available':True}
-    def __init__(self, limit=10, max_offset = 200):
+    def __init__(self, limit=10, max_offset = 1000):
         if self.__singleton:
             #self.instance()
             return
-        
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.__fill_products(limit, max_offset))
+
+    async def __fill_products(self, limit, max_offset):
+        loop = asyncio.get_event_loop()
         def params_gen():
             params = {'limit': limit, 'offset': 0}
             for off in range(max_offset // limit):
@@ -25,8 +30,14 @@ class Products:
         self.products = {}
 
         tmp = []
+        futures = []
         for params in params_gen():
-            resp = requests.get(self.__url, params = params)
+            future = loop.run_in_executor(None, functools.partial(requests.get, self.__url, params = params))
+            futures.append(future)
+
+        for future in futures:
+            resp = await future
+            #resp = requests.get(self.__url, params = params)
             json = resp.json()
             for prd in json:
                 if all([prd[key] == val for key, val in self.__json_tokens.items()]):
