2a275f140f3fc8ffc45f3b6b08b43485feb7733b
shurik <shurik@skotch.nirvana>
2022-06-07 20:56:39 +0300

    kaka

--- nasa_api/main.py
+++ nasa_api/main.py
@@ -6,14 +6,18 @@ import password
 def name_gen():
     count = 0
     while True:
-        yield f"r003_{count}"
+        yield f"s001_{count}"
         count += 1
 
 def main():
     token = nasa_api.Token(password.login, password.password)
     #print(token.t())
-    r = 0.005
+    r = 0.002
     size = 1000
+    nasa_api.Sphere.lat_start = 0.5292036732051038- 0.12
+    nasa_api.Sphere.lon_start = 0.65
+    nasa_api.Sphere.lat_end = 0.5292036732051038+ 0.12
+    nasa_api.Sphere.lon_end = 2
     s = nasa_api.Sphere(r, r, size)
     #p = nasa_api.Products.instance(10000).products
     #prod_id = next(iter(p))
